92be73ee738004b046a462680b5a3f8555f0fb8a
Krani <kranicz.tamas91@gmail.com>
2020-09-22 21:48:03 +0200

    add timedate check before writing new data

--- bandi.py
+++ bandi.py
@@ -3,7 +3,10 @@ import requests as req
 import os
 import sys
 import xml.etree.ElementTree as ET
+import re
+import datetime
 
+# configuration
 xml_url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
 tempfile = "temp-currencies.xml"
 datafile = "currencies.csv"
@@ -23,21 +26,44 @@ def get_xml_data(url):
 def open_output_file(fname):
     # open output csv file
     outfile_is_existing = os.path.isfile(fname)
-    fp = open(fname, "a")
+    fp = open(fname, "a+")
     if not outfile_is_existing:
         # print csv header first if file is not existing
         fp.write("date;EUR-HUF;EUR-USD;EUR-CHF;EUR-JPY\n")
     return fp
 
+def get_last_line(fp):
+    # return with the last line of the file pointed by fp
+    fp.seek(0,0)
+    for last_line in fp:
+        pass
+    print("last line: ", last_line)
+    return last_line
+
 def write_daydata(fp, d):
     # todo: check if data is 'fresh enough' to write and not already in
+    up_to_date = False
     line = ""
+    now = datetime.datetime.now()
+    now = int(now.strftime("%Y%m%d"))
+    print("@ now: ", now)
+    lastl = get_last_line(fp)
+    last_date = re.match(r'^\d{4}-\d{2}-\d{2}', lastl)
+    if last_date:
+        last_date = last_date.group(0)
+        last_date = int(last_date.replace('-', ''))
+        print("@ last_date: ", last_date)
+        if last_date >= now:
+            up_to_date = True
+
     for val in d.values():
         line += val + ';'
 
     line = line[:-1] + '\n'
-    print("line:", line)
-    fp.write(line)
+    print("@ line to be written:",line)
+    if not up_to_date:
+        # write it only if the data is not up to date
+        fp.write(line)
     return
 
 def get_daydata(root):

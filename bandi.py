dba5b014b96d238e1edc13cd58feaee4e62a859f
Krani <kranicz.tamas91@gmail.com>
2020-10-05 14:54:15 +0200

    Fix timedate compare and remove debug prints

--- bandi.py
+++ bandi.py
@@ -19,7 +19,6 @@ def get_xml_data(url):
     if len(resp.content) > 2000:
         print("xml input file is larger than 2000 byte!")
         sys.exit(2)
-    print("xml content length:", len(resp.content))
     return resp.content.decode('utf-8')
 
 def open_output_file(fname):
@@ -27,7 +26,7 @@ def open_output_file(fname):
     outfile_is_existing = os.path.isfile(fname)
     fp = open(fname, "a+")
     if not outfile_is_existing:
-        # print csv header first if file is not existing
+        # create csv header first if the file did not exist before
         fp.write("date;EUR-HUF;EUR-USD;EUR-CHF;EUR-JPY\n")
     return fp
 
@@ -36,39 +35,39 @@ def get_last_line(fp):
     fp.seek(0,0)
     for last_line in fp:
         pass
-    print("last line: ", last_line)
     return last_line
 
 def write_daydata(fp, d):
-    # todo: check if data is 'fresh enough' to write and not already in
     up_to_date = False
     line = ""
-    now = datetime.datetime.now()
-    now = int(now.strftime("%Y%m%d"))
-    print("@ now: ", now)
+    new_data_date = d['date']
+    new_data_date = int(new_data_date.replace('-', ''))
+
     lastl = get_last_line(fp)
     last_date = re.match(r'^\d{4}-\d{2}-\d{2}', lastl)
     if last_date:
         last_date = last_date.group(0)
         last_date = int(last_date.replace('-', ''))
-        print("@ last_date: ", last_date)
-        if last_date >= now:
+
+        # Check if available data is up to date
+        if last_date >= new_data_date:
             up_to_date = True
 
     for val in d.values():
         line += val + ';'
 
     line = line[:-1] + '\n'
-    print("@ line to be written:",line)
     if not up_to_date:
         # write it only if the data is not up to date
         fp.write(line)
+    else:
+        print("Data is up-to date, no data written!")
     return
 
 def get_daydata(root):
     money = root[2][0]
     timedate = root[2][0].attrib['time']
-    print("timedate for the data: ", timedate)
+
     l = []
     for child in money:
         l.append(child.attrib)
@@ -76,7 +75,6 @@ def get_daydata(root):
     daydata = {'date': '', 'huf': 0, 'usd' : 0, 'chf' : 0, 'jpy' : 0}
     daydata['date'] = timedate
     for item in l:
-        print(item)
         if item['currency'] == 'HUF':
             daydata['huf'] = item['rate']
         elif item['currency'] == 'USD':
@@ -102,9 +100,6 @@ def main():
 
     write_daydata(outf, dayd)
     outf.close()
-    print(dayd)
-
-    print("Hello!")
 
 if __name__ == '__main__':
     main()
